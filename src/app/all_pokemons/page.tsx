d6775ac072676a93ea5587242a041c3e0453b686
pieterjansen8 <pieterjansen8@icloud.com>
2025-01-03 02:05:32 +0100

    added all pokemons, with great perfomance, switched to swr, better perfomance, ' '

---
+++ src/app/all_pokemons/page.tsx
@@ -0,0 +1,144 @@
+"use client";
+import useSWR, { mutate } from "swr";
+import { useState, useEffect, useCallback } from "react";
+import { motion } from "framer-motion";
+import { Loader2 } from "lucide-react";
+import React from "react";
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+export default function Home() {
+  const [limit, setLimit] = useState(30);
+  const [pokemonList, setPokemonList] = useState<any[]>([]);
+  const { data, error, isLoading } = useSWR(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}`,
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      setPokemonList((prevList) => [...prevList, ...data.results]);
+    }
+  }, [data]);
+
+  const handleScroll = useCallback(() => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 2) {
+      setLimit((prevLimit) => {
+        const newLimit = prevLimit + 30 > 1025 ? 1025 : prevLimit + 30;
+        if (newLimit > 60) {
+          mutate(`https://pokeapi.co/api/v2/pokemon?limit=${newLimit - 60}`, undefined, { revalidate: false });
+        }
+        return newLimit;
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  if (error) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-gray-100 p-4">
+        <span className="text-2xl text-white text-center">
+          ERROR: {(error as Error).message}
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-center mb-4">All Pokemons</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {pokemonList.map((pokemon: any, index: number) => (
+          <Pok img={index + 1} key={index} />
+        ))}
+      </div>
+      {isLoading && (
+        <div className="flex items-center justify-center mt-4">
+          <Loader2 className="w-12 h-12 text-white animate-spin" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+const Pok = React.memo(({ img }: { img: number }) => {
+  if (img > 1025 || img < 1) {
+    return(
+        <motion.div
+            className="w-full h-full flex items-center justify-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+        >
+            <span className="text-2xl text-white text-center">
+              No Pokemons available above 1025 (:
+            </span>
+        </motion.div>
+    )
+  }
+  const { data, error, isLoading } = useSWR(
+    `https://pokeapi.co/api/v2/pokemon/${img}`,
+    fetcher
+  );
+  if (error) {
+    return (
+      <motion.div
+        className="w-full h-full flex items-center justify-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <span className="text-2xl text-white text-center">
+          ERROR: {(error as Error).message}
+        </span>
+      </motion.div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <motion.div
+        className="w-full h-full flex items-center justify-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <Loader2 className="w-12 h-12 text-white animate-spin" />
+      </motion.div>
+    );
+  }
+
+  if (data) {
+    return (
+      <motion.div
+        className="flex flex-col h-full"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h2 className="text-3xl font-bold mb-4 text-center capitalize text-white">
+          {data.forms[0].name}
+        </h2>
+        <div className="flex-grow flex items-center justify-center bg-gray-700 rounded-xl overflow-hidden mb-4 p-4">
+          <motion.img
+            className="max-w-full max-h-full object-contain filter drop-shadow-lg"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            alt={`Pokemon ${img}`}
+            src={data.sprites.other.home.front_default}
+          />
+        </div>
+        <span className="text-xl text-center font-semibold text-gray-300">
+          Pokémon #{img}
+        </span>
+      </motion.div>
+    );
+  }
+
+  return null;
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,84 @@
-'use client'
+"use client";
 
-import { useState } from "react"
-import { useQuery, QueryClientProvider, QueryClient } from "@tanstack/react-query"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input" 
-import { ArrowRight, ArrowLeft, Loader2 } from 'lucide-react'
-import { motion } from "framer-motion"
-import Link from "next/link"
-const queryClient = new QueryClient()
-
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ArrowRight, ArrowLeft, Loader2 } from "lucide-react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import useSWR from "swr";
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 export default function Home() {
-  const [image, setImage] = useState(1)
+  const [image, setImage] = useState(1);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-gray-100 p-4">
-        <motion.h1 
-          className="text-5xl font-bold mb-12 text-white"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-gray-100 p-4">
+      <motion.h1
+        className="text-5xl font-bold mb-12 text-white"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        Fetch your Pokémon
+      </motion.h1>
+      <motion.div
+        className="w-full max-w-md aspect-square mb-8 bg-gray-800 rounded-2xl p-6 shadow-lg flex flex-col overflow-hidden"
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Pok img={image} />
+      </motion.div>
+      <div className="flex gap-4">
+        <Button
+          onClick={() => setImage((old) => Math.max(1, old - 1))}
+          className="bg-slate-800 hover:bg-slate-700 text-white transition-all duration-300 ease-in-out transform hover:scale-105"
         >
-          Fetch your Pokémon
-        </motion.h1>
-        <motion.div 
-          className="w-full max-w-md aspect-square mb-8 bg-gray-800 rounded-2xl p-6 shadow-lg flex flex-col overflow-hidden"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+          <ArrowLeft className="mr-2" />
+          Previous
+        </Button>
+        <Button
+          onClick={() => setImage((old) => old + 1)}
+          className="bg-slate-800 hover:bg-slate-700 text-white transition-all duration-300 ease-in-out transform hover:scale-105"
         >
-          <Pok img={image} />
-        </motion.div>
-        <div className="flex gap-4">
-          <Button 
-            onClick={() => setImage(old => Math.max(1, old - 1))}
-            className="bg-slate-800 hover:bg-slate-700 text-white transition-all duration-300 ease-in-out transform hover:scale-105"
-          >
-            <ArrowLeft className="mr-2" />
-            Previous
-          </Button>
-          <Button 
-            onClick={() => setImage(old => old + 1)}
-            className="bg-slate-800 hover:bg-slate-700 text-white transition-all duration-300 ease-in-out transform hover:scale-105"
-          >
-            Next
-            <ArrowRight className="ml-2" />
-          </Button>
-        </div>
-        <div className="flex-grow mt-4">
-          <Input type="number" onChange={(e) => {setImage(Number(e.target.value))}} placeholder="Pokemon id"></Input>
-        </div>
-        <span className="float-start mt-6 text-xl">created by <Link href={"https://github.com/pieterjansen8"} className="text-blue-500 underline" >pieter jansen</Link> </span>
+          Next
+          <ArrowRight className="ml-2" />
+        </Button>
       </div>
-    </QueryClientProvider>
-  )
+      <div className="flex-grow mt-4">
+        <Input
+          type="number"
+          onChange={(e) => {
+            setImage(Number(e.target.value));
+          }}
+          placeholder="Pokemon id"
+        ></Input>
+      </div>
+      <span className="float-start mt-6 text-xl">
+        <Link
+          href={"/all_pokemons"}
+          className="text-blue-500 underline"
+        >
+          All pokemons
+        </Link>{" "}
+      </span>
+      <span className="float-start mt-6 text-xl">
+        created by{" "}
+        <Link
+          href={"https://github.com/pieterjansen8"}
+          className="text-blue-500 underline"
+        >
+          pieter jansen
+        </Link>{" "}
+      </span>
+    </div>
+  );
 }
 
 function Pok({ img }: { img: number }) {
-  const { data, status, error } = useQuery({
-    queryKey: [img],
-    queryFn: async () => {
-      return await fetch(`https://pokeapi.co/api/v2/pokemon/${img}`).then(res => res.clone().json())
-    }
-  })
-  if(img>1025){
-    return(
-      <motion.div 
+  if (img > 1025 || img < 1) {
+    return (
+      <motion.div
         className="w-full h-full flex items-center justify-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -74,12 +87,16 @@ function Pok({ img }: { img: number }) {
         <span className="text-2xl text-white text-center">
           ERROR: Pokemon not found
         </span>
-       </motion.div>
-    )
+      </motion.div>
+    );
   }
-  if (status === "error") {
+  const { data, error, isLoading } = useSWR(
+    `https://pokeapi.co/api/v2/pokemon/${img}`,
+    fetcher
+  );
+  if (error) {
     return (
-      <motion.div 
+      <motion.div
         className="w-full h-full flex items-center justify-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -89,12 +106,12 @@ function Pok({ img }: { img: number }) {
           ERROR: {(error as Error).message}
         </span>
       </motion.div>
-    )
+    );
   }
 
-  if (status === "pending") {
+  if (isLoading) {
     return (
-      <motion.div 
+      <motion.div
         className="w-full h-full flex items-center justify-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -102,12 +119,12 @@ function Pok({ img }: { img: number }) {
       >
         <Loader2 className="w-12 h-12 text-white animate-spin" />
       </motion.div>
-    )
+    );
   }
 
-  if (status === "success") {
+  if (data) {
     return (
-      <motion.div 
+      <motion.div
         className="flex flex-col h-full"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
@@ -117,20 +134,21 @@ function Pok({ img }: { img: number }) {
           {data.forms[0].name}
         </h2>
         <div className="flex-grow flex items-center justify-center bg-gray-700 rounded-xl overflow-hidden mb-4 p-4">
-          <motion.img 
+          <motion.img
             className="max-w-full max-h-full object-contain filter drop-shadow-lg"
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
-            alt={`Pokemon ${img}`} 
+            alt={`Pokemon ${img}`}
             src={data.sprites.other.home.front_default}
           />
         </div>
-        <span className="text-xl text-center font-semibold text-gray-300">Pokémon #{img}</span>
+        <span className="text-xl text-center font-semibold text-gray-300">
+          Pokémon #{img}
+        </span>
       </motion.div>
-    )
+    );
   }
 
-  return null
+  return null;
 }
-
